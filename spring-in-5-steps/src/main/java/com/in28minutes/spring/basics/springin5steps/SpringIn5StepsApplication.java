a84066785417a1b3b75141c886a363ae39e966e9
Nichol Dimalanta <nichol.dimalanta@orangeandbronze.com>
2021-11-15 10:15:14 +0800

    dependency injection via Spring
    sort set to bubbleSort, default quickSort

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,15 +2,17 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
 
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		binarySearch.setSortAlgorithm(new BubbleSortAlgorithm());
 		System.out.println(binarySearch.binarySearch(new int[]{5, 12, 8, 9}, 3));
-		SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 
 }
