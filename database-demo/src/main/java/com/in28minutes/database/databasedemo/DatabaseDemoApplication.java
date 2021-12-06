ad409bd47f0b9e6e76f2261524c2ee06a24c4c3a
Nichol Dimalanta <nichol.dimalanta@orangeandbronze.com>
2021-12-06 11:33:08 +0800

    jdbc intro

---
+++ database-demo/src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -0,0 +1,45 @@
+package com.in28minutes.database.databasedemo;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDao;
+import java.util.Collection;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private PersonJdbcDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		final Collection<Person> personById = dao.findAll();
+		logger.info("People({}): {}", personById.size(), personById);
+
+		final int personId = 10002;
+		logger.info("Person id#{}: {}", personId, dao.findById(personId));
+
+		final String personName = "Joe Mama";
+		final Collection<Person> personByName = dao.findByName(personName);
+		logger.info("Named '{}'({}): {}", personName, personByName.size(), personByName);
+
+		logger.info("Delete Person id#{}: {}", personId, dao.deleteById(personId));
+
+		Person newPerson = new Person(10005, "Pietra", "Russia", new Date());
+		logger.info("Insert Person {}: {}", newPerson, dao.insert(newPerson));
+
+		logger.info("Renamed {}, {}", newPerson.getId(), dao.updateNameById(newPerson.getId(), "Boskonovich"));
+		newPerson.setLocation("Kamuning");
+		logger.info("Updated Person again to {}, {}", newPerson, dao.update(newPerson));
+	}
+}
---
+++ database-demo/src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,49 @@
+package com.in28minutes.database.databasedemo.jdbc;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PersonJdbcDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private BeanPropertyRowMapper<Person> createPersonRowMapper() {
+        return new BeanPropertyRowMapper<Person>(Person.class);
+    }
+
+    public Collection<Person> findAll() {
+        return jdbcTemplate.query("SELECT * FROM Person", createPersonRowMapper());
+    }
+
+    public Person findById(int id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM Person WHERE id = ?", createPersonRowMapper(), new Object[]{id});
+    }
+
+    public Collection<Person> findByName(String name) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE name = ?", createPersonRowMapper(), new Object[]{name});
+    }
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update("DELETE FROM Person WHERE id = ?", id);
+    }
+
+    public int insert(Person person) {
+        return jdbcTemplate.update("INSERT INTO Person(id, name, location, birth_date) " +
+                "VALUES (?,?,?,?)", person.getId(), person.getName(), person.getLocation(), person.getBirthDate());
+    }
+
+    public Object updateNameById(int id, String name) {
+        return jdbcTemplate.update("UPDATE Person SET name = ? WHERE id = ?", new Object[]{name, id});
+    }
+
+    public int update(Person person) {
+        return jdbcTemplate.update("UPDATE Person SET name = ?, location = ?, birth_date = ?  WHERE id = ?",
+                person.getName(), person.getLocation(), person.getBirthDate(), person.getId());
+    }
+}
